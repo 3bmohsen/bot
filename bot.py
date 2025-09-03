69def944cac337bc9c20f865c594f823c4647024
Ahmed Mohsen <73487564+3bmohsen@users.noreply.github.com>
2025-09-04 02:40:36 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,60 @@
+import requests
+import time
+
+url = "https://tms.tabadul.sa/api/appointment/tas/v2/zone/schedule/land"
+params = {
+    "departure": "KFC",
+    "arrival": "31",
+    "type": "TRANSIT",
+    "economicOperator": ""
+}
+
+headers = {
+    "Accept": "application/json",
+    "Accept-Language": "ar",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Content-Type": "application/json; charset=utf-8",
+    "Origin": "https://oga.fasah.sa",
+    "Pragma": "no-cache",
+    "Referer": "https://oga.fasah.sa/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-GPC": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Not;A=Brand";v="99", "Brave";v="139", "Chromium";v="139"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "token": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYWx1ZHMwMDE2MCIsIkdST1VQUyAiOiJUQVNHRU4sVFJOUFJULFRSTlBSVEYsVFJOUFJURkksVFJBTlNQT1JUX0FHRU5UIiwiR1JPVVBTIjoiVEFTR0VOLFRSTlBSVCxUUk5QUlRGLFRSTlBSVEZJLFRSQU5TUE9SVF9BR0VOVCIsIklTX1NTTyAiOmZhbHNlLCJTU09fVE9LRU4gIjoiIiwiSVNfU1NPIjpmYWxzZSwiU1NPX1RPS0VOIjoiIiwiQ0xJRU5UX05BTUUiOiJGQVNBSCIsImlzcyI6IkZBU0FIIiwiYXVkIjoiRkFTQUggQXBwbGljYXRpb24iLCJleHAiOjE3NTY5NjgwOTF9.0UFfGxMqll1bHErASb9GOZw3RgZWfZd69h5Y-hlOuuvdXAu-t-IoNcMUxaLyourL2ehThH7-biVNp862gFiqJQ"
+}
+
+while True:
+    try:
+        response = requests.get(url, headers=headers, params=params, timeout=30)
+        print("Status:", response.status_code)
+
+        if response.status_code == 200:
+            data = response.json()
+            print("Response data:", data)
+
+            # لو الرسالة بتقول "لا يوجد مواعيد متاحة" → كمل
+            if (
+                not data.get("success")
+                and data.get("errors")
+                and data["errors"][0].get("message") == "لا يوجد مواعيد متاحة"
+            ):
+                print("⚠️ مفيش مواعيد متاحة، هنجرب تاني بعد دقيقة...")
+            
+            else:
+                # أي رد غير كده يبقى نجحنا
+                print("✅ لقيت رد مختلف، وقفنا التكرار.")
+                print("Final data:", data)
+                break
+        else:
+            print("❌ Error:", response.text)
+
+    except Exception as e:
+        print("Error:", e)
+
+    time.sleep(60)  # انتظر دقيقة
